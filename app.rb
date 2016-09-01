1f46262f935ef42ef859fd26fda144b6f78f854b
Араик Акопян <araikakopan@MacBook-Pro-Araik.local>
2016-09-02 00:34:41 +0300

    Execute simple query

--- app.rb
+++ app.rb
@@ -1,3 +1,6 @@
 require 'sqlite3'
 
-db = SQlite3::Database.new 'mydatabase.sqlite'
+db = SQLite3::Database.open 'mydatabase.sqlite'
+
+db.execute "INSERT INTO Cars (Name, Price) VALUES ('Jaguar', '7777')"
+db.close
